a9cbaa47b3f69d189cbcf8e092d2ce5b45445da9
AbhijeetAlshi-Insight-Code <42849346+AbhijeetAlshi-Insight-Code@users.noreply.github.com>
2018-08-30 17:03:39 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+python get-pip.py
+pip install pandas
+pip isntall sys
 python ./src/average-error.py ./input/actual.txt ./input/predicted.txt ./input/window.txt ./output/comparison.txt
